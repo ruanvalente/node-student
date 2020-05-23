8c5ac8d4f481c50e3a8c178596f62c49c019b83e
ruanvalente <ruanopensuselinux@gmail.com>
2020-05-23 11:18:57 -0300

    refactoring userController

--- node-api/src/controllers/userController.js
+++ node-api/src/controllers/userController.js
@@ -5,23 +5,46 @@ const User = mongoose.model('User')
 
 module.exports = {
   async index(req, res) {
-    const user = await User.find({})
-    return res.json(user)
+    try {
+      const user = await User.find({})
+      if(user.length === 0) {
+        return res.status(200).json({ message: 'Nenhum usuário criado' })
+      }
+      return res.json(user)
+    } catch (e) {
+      return res.status(500).json({ message: 'Falha ao listar os usuários' })
+    }
   },
   async create(req, res) {
-    const user = await User.create(req.body)
-    return res.json(user)
+    try {
+      const user = await User.create(req.body)
+      return res.status(201).json(user)
+    } catch (e) {
+      return res.status(500).json({ message: 'Falha ao criar usuário' })
+    }
   },
   async show(req, res) {
-    const user = await User.findById(req.params.id)
-    return res.json(user)
+    try {
+      const user = await User.findById(req.params.id)
+      return res.status(200).json(user)
+    } catch (e) {
+      return res.status(404).json( {message: 'Usuário não encontrado'} )
+    }
   },
   async update(req, res) {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    return res.json(user)
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      return res.status(200).json(user)
+    } catch (e) {
+      return res.status(500).json({ message: 'Falha ao atualizar o registro' })
+    }
   },
   async destroy(req, res) {
-    await User.findByIdAndDelete(req.params.id, req.body)
-    return res.json({ message: 'User is remove'})
+    try {
+      await User.findByIdAndDelete(req.params.id, req.body)
+      return res.status(200).json({ message: 'Usuário removido'})
+    } catch (e) {
+      return res.status(500).json({ message: 'Falha ao ao remover usuário' })
+    }
   }
 }
